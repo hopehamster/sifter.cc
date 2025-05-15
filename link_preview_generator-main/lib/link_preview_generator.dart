e20d085747e6548438ad936c04067cd3c853e450
hopehamster <mikebradley1980@gmail.com>
2025-05-14 23:32:09 -0700

    Delete link_preview_generator-main directory

--- link_preview_generator-main/lib/link_preview_generator.dart
+++
@@ -1,5 +0,0 @@
-library link_preview_generator;
-
-export 'src/models/types.dart';
-export 'src/utils/scrapper.dart' show LinkPreview;
-export 'src/widgets/link_preview.dart' show LinkPreviewGenerator;
